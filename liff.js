460555ad0764ca003bb37d1105db17c1921e2826
Sitthi Thiammekha <sitthi@alphaemgroup.com>
2019-07-18 14:23:21 +0700

    USER_SERVICE_UUID

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '85d809a9-a2f8-4511-8ab4-d941fcb94601'; // LED, Button
+const USER_SERVICE_UUID         = 'f46452e1-0fd3-467a-9454-631b61106772'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
